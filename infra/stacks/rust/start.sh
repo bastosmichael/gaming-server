a4f98251023e447e5e2c53d3b46f9b993495445e
Michael Bastos <bastosmichael@gmail.com>
2026-02-09 13:57:12 -0600

    updating world size

--- infra/stacks/rust/start.sh
+++ infra/stacks/rust/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+echo "Cleaning bundles to ensure integrity..."
+rm -rf /steamcmd/rust/game/Bundles
+rm -rf /steamcmd/rust/game/bundles
+
 echo "Updating Rust server..."
 # Update and validate the server files
 # app_id 258550 is Rust
@@ -9,8 +13,8 @@ steamcmd +force_install_dir /steamcmd/rust/game +login anonymous +app_update 258
 echo "Starting Rust server..."
 # Default environment variables
 : ${RUST_SERVER_IDENTITY:="docker"}
-: ${RUST_SERVER_SEED:="12345"}
-: ${RUST_SERVER_WORLDSIZE:="4500"}
+: ${RUST_SERVER_SEED:="1337"}
+: ${RUST_SERVER_WORLDSIZE:="3000"}
 : ${RUST_SERVER_NAME:="Rust Server"}
 : ${RUST_SERVER_MAXPLAYERS:="10"}
 : ${RUST_RCON_PORT:="28016"}
